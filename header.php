93c435aa0837e7ab3d629b9e08715c06c3e3732d
slim16165 <hyperreview@gmail.com>
2020-08-22 23:18:20 +0200

    Added Adsense script

--- header.php
+++ header.php
@@ -19,6 +19,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
+
+    
+
+    <!-- EZOIC_REMOVE_BEGIN -->
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-4057480177165624",
+            enable_page_level_ads: true
+        });
+    </script>
+    <!-- EZOIC_REMOVE_END -->
+
+
+
 </head>
 <body <?php body_class(); ?>>
 
